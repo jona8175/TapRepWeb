ebc379022dbae777a878222d3461ebf8a8ccd177
jona8175 <jonas.kubik@uzh.ch>
2024-11-24 20:43:35 +0100

    Formen Texte ergäntzt

--- src/routes/blockdiagram/+page.svelte
+++ src/routes/blockdiagram/+page.svelte
@@ -12,31 +12,31 @@
       position: new THREE.Vector3(-0.17, 0.22, -0.1),
       canvasCordinates: new THREE.Vector2(3, 0),
       label: "Toteis",
-      text: "Text",
+      text: "Wenn Gletscher abschmelzen, bleiben oft isolierte Eisblöcke als sogenanntes Toteis zurück. Diese schmelzen teilweise langsam ab während sich der Gletscher weiter zurückzieht. ",
     },
-    /*{
+    {
       position: new THREE.Vector3(-0.05, 0.12, 0.1),
       canvasCordinates: new THREE.Vector2(3, 0),
       label: "Toteis-See",
-      text: "Text",
-    },*/
+      text: "Die Hohlform welche von Toteisblöcken zurückgelassen wird kann sich mit Wasser füllen. ",
+    },
     {
       position: new THREE.Vector3(0, 0.6, 0.25),
       canvasCordinates: new THREE.Vector2(3, 0),
       label: "Gletscher Vorfeld",
-      text: "Text",
+      text: "Gletschervorfeld bezeichnet das Gelände zwischen dem Heutigen Gletscherrand und den Moränen des 1850 Vorstosses. Dieses Gebiet ist oft dominiert vom Schutt der Grundmoräne Vegetation ist erst auf dem Vormarsch. Der Formenschatz umfasst Bildungen wie Sander, Toteis, Rundhöcker und Drumlins.",
     },
     {
       position: new THREE.Vector3(0.28, 0.22, -0.1),
       canvasCordinates: new THREE.Vector2(3, 0),
       label: "Rundhöcker",
-      text: "Text",
+      text: "Längliche Hügel, aus dem im Untergrund-Gestein werden als Rundhöcker bezeichnet. Sie wurden durch das vorrückende Eis abgeschliffen. ",
     },
     {
       position: new THREE.Vector3(0.8, 0.15, 0.1),
       canvasCordinates: new THREE.Vector2(3, 0),
       label: "Massenbewegung",
-      text: "Text",
+      text: "In sogenannten Massenbewegungen Ereignissen werden grosse Mengen von Material Talabwärts bewegt. Solche es gibt sehr viele verschiedene Arten von Ereignissen wie Bergstürze, oder Murgänge die wiederum zu unzähligen verschiedenen Formen wie Sturzhalden, Lawinenhalden und Murgang Akkumulationen führen. Diese Events und formen werden von verschiedenen Autoren nach Grösse, Geschwindigkeit, involviertem Material oder weiteren Faktoren klassifiziert.",
     },
     {
       position: new THREE.Vector3(-0.25, 0.25, 0.4),
